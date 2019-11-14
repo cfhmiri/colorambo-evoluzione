cafc7e99cdfd33fdcc9fc98c8eed4dc29028346f
Andrea Mirandola <andrea.mirandola.99@gmail.com>
2019-11-14 14:50:13 +0100

    create DB

---
+++ PHP/db_utils.php
@@ -0,0 +1,46 @@
+<?php
+    class DB{
+        function DBconnection(){
+            $conn = mysqli_connect("localhost", "colorambo_evoluzione", "password", "colorambo_evoluzione");
+            if(!$conn){
+                echo "errore";
+            } else {
+                echo "andata";
+            }
+            return $conn;
+        }
+    
+
+        function getUsers($conn){
+           $queryUsers = "SELECT * FROM users";
+           $queryFetch = $conn->query($queryUsers);
+           $result = $queryFetch->fetch_all();
+           echo "<br>";
+           var_dump ($result);
+           
+        }
+        function getColors($conn){
+            $queryUsers = "SELECT * FROM colors";
+            $queryFetch = $conn->query($queryUsers);
+            $result = $queryFetch->fetch_assoc();
+            echo "<br>";
+            echo $result['name'];
+            
+         }
+        function getRoles($conn){
+            $queryUsers = "SELECT * FROM roles";
+            $queryFetch = $conn->query($queryUsers);
+            $result = $queryFetch->fetch_assoc();
+            echo "<br>";
+            echo $result['role_name'];
+        }
+        function getFavorities($conn){
+            $queryUsers = "SELECT * FROM favorities";
+            $queryFetch = $conn->query($queryUsers);
+            $result = $queryFetch->fetch_assoc();
+            echo "<br>";
+            echo $result['id'];
+            
+        }
+    }
+?>
