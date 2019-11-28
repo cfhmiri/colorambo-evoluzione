273b176f88244d11213cbe61767661571abdfbef
Andrea Mirandola <andrea.mirandola.99@gmail.com>
2019-11-28 17:58:19 +0100

    modifiche db

--- PHPGlorioso/createDB.php
+++ PHPGlorioso/createDB.php
@@ -17,5 +17,26 @@ $sql = "CREATE DATABASE colorambo_glorioso";
 //     echo "Errore creazione DB" . mysqli_error($conn);
 // }
 
+// mysqli_close($conn);
+
+$db_selected = mysqli_select_db('colorambo_glorioso', $conn);
+if (!$db_selected) {
+    die ('Can\'t use colorambo_glorioso : ' . mysqli_error());
+}
+
+//Creazione Tabelle
+$sql= "CREATE TABLE `colors` (
+    `id_color` int(11) UNSIGNED NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `code` varchar(7) DEFAULT NULL,
+    `creator_id` int(10) UNSIGNED DEFAULT NULL
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1";
+
+    if ($conn->query($sql) === TRUE){
+        echo "Tabella COLORI creata";
+    }else {
+        echo "errore " . $conn->error;
+    }
+   
 mysqli_close($conn);
 ?>
