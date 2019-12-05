a56aefc2e7076d256f0ad557dd3ae724a4c34eb7
Andrea Mirandola <andrea.mirandola.99@gmail.com>
2019-12-05 15:53:58 +0100

    creazioni tabelle

--- PHPGlorioso/createDB.php
+++ PHPGlorioso/createDB.php
@@ -25,61 +25,52 @@ if (!$db_selected) {
 }
 
 //Creazione Tabelle
-$sql= "CREATE TABLE colors (
-    id_color int(11) NOT NULL AUTO_INCREMENT,
-    name varchar(255) NOT NULL,
-    code varchar(7) DEFAULT NULL,
-    user_id int(10) NOT NULL,
-    PRIMARY KEY (id_color),
-    FOREIGN KEY (user_id) REFERENCES users(id_user)
-  )";
-
-$sql= "CREATE TABLE users (
-    id_user int(11) NOT NULL AUTO_INCREMENT,
-    f_name varchar(255) NOT NULL,
-    l_name varchar(255) DEFAULT NULL,
-    birth_date date(15) DEFAULT NULL,
-    username varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
-    role_id varchar(20) NOT NULL,
-    PRIMARY KEY (id_user),
-    FOREIGN KEY (role_id) REFERENCES roles(id_role)
+$sql= "CREATE TABLE `colors` (
+    `id_color` int(11) NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `code` varchar(7) DEFAULT NULL,
+    `user_id` int(10) NOT NULL
   )";
 
 
-$sql= "CREATE TABLE roles (
-    id_role int(11) NOT NULL AUTO_INCREMENT,
-    role_name varchar(255) NOT NULL,
-    PRIMARY KEY (id_role)
+
+$sql= "CREATE TABLE `users` (
+    `id` int(11) NOT NULL,
+    `f_name` varchar(255) NOT NULL,
+    `l_name` varchar(255) DEFAULT NULL,
+    `birth_date` datetime DEFAULT NULL,
+    `username` varchar(255) NOT NULL,
+    `password` varchar(255) NOT NULL,
+    `role_id` varchar(20) NOT NULL
   )";
 
-$sql="CREATE TABLE stats (
-    id_stat int(11) NOT NULL AUTO_INCREMENT,
-    num_colors int(90) NOT NULL,
-    num_login int(255) DEFAULT NULL,
-    num_pages int(15) DEFAULT NULL,
-    user_id varchar(255) NOT NULL,
-    color_id varchar(255) NOT NULL,
-    role_id varchar(20) NOT NULL,
-    PRIMARY KEY (id_stat),
-    FOREIGN KEY (user_id) REFERENCES users(id_user),
-    FOREIGN KEY (color_id) REFERENCES colors(id_color),
-    FOREIGN KEY (role_id) REFERENCES roles(id_role)
+
+
+$sql= "CREATE TABLE `roles` (
+    `id` int(11) NOT NULL,
+    `role_name` varchar(255) NOT NULL
   )";
 
-$sql= "CREATE TABLE favorites (
-    id_fav int(11) NOT NULL AUTO_INCREMENT,
-    date date(255) NOT NULL,
-    color_id varchar(7) DEFAULT NULL,
-    user_id int(10) DEFAULT NULL,
-    PRIMARY KEY (id_fav),
-    FOREIGN KEY (user_id) REFERENCES users(id_user),
-    FOREIGN KEY (color_id) REFERENCES colors(id_color)
-  )";
+$sql="CREATE TABLE `stats` (
+    `id` int(11) NOT NULL,
+    `num_colors` int(90) NOT NULL,
+    `num_login` int(255) DEFAULT NULL,
+    `num_pages` int(15) DEFAULT NULL,
+    `user_id` varchar(255) NOT NULL,
+    `color_id` varchar(255) NOT NULL,
+    `role_id` varchar(20) NOT NULL
+      )";
+
+ $sql= "CREATE TABLE `favorites` (
+     `id` int(11) NOT NULL,
+     `date` datetime NOT NULL,
+     `color_id` varchar(7) DEFAULT NULL,
+     `user_id` int(10) DEFAULT NULL
+     )";
 
 
     if ($conn->query($sql) === TRUE){
-        echo "Tabella COLORI creata";
+        echo "Tabella" .  $sql . "creata";
     }else {
         echo "errore " . $conn->error;
     }
